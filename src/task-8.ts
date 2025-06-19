6e821834e81da9302bcdf96b00a65b1d9b1cf67e
RedGfox <zim238kosmit@gmail.com>
2025-06-19 12:08:37 +0300

    End-HW

--- src/task-8.ts
+++ src/task-8.ts
@@ -1,12 +1,18 @@
-// import axios from "axios";
+import axios from "axios";
 
-// async function fetchPosts() {
-//   const response = await axios.get(
-//     "<https://jsonplaceholder.typicode.com/posts>"
-//   );
-//   return response.data;
-// }
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
 
-// fetchPosts().then((posts) => {
-//   console.log(posts[0].title);
-// });
+async function fetchPosts(): Promise<Post[]> {
+  const response = await axios.get<Post[]>(
+    "<https://jsonplaceholder.typicode.com/posts>"
+  );
+  return response.data;
+}
+
+fetchPosts().then((posts) => {
+  console.log(posts[0].title);
+});
